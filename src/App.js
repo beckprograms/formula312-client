b1a479d8e6dd85310ca71f9c5c9f8f12fdf533d4
Chaithanya, Penumajji <sivachaitra.369@gmail.com>
2022-12-17 09:10:16 +0530

    Refactored login and setuped eslint

--- src/App.js
+++ src/App.js
@@ -1,32 +1,38 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
+import { Amplify } from "aws-amplify";
+import { useAuthenticator } from "@aws-amplify/ui-react";
+
+import { AppAuthenticator } from "./components/AppAuthenticator";
 import LandingContainer from "./components/LandingContainer";
 import { Details } from "./components/Details";
+
 import "./App.css";
-
-import { Amplify } from "aws-amplify";
-
-import { Authenticator } from "@aws-amplify/ui-react";
-import "@aws-amplify/ui-react/styles.css";
 
 import awsExports from "./aws-exports";
 Amplify.configure(awsExports);
 
-function App() {
+const Home = () => {
+  const { signOut } = useAuthenticator((context) => [context.user]);
+
   return (
-    <Authenticator>
-      {({ signOut, user }) => (
-        <React.Fragment>
-          <Routes>
-            <Route
-              path="/"
-              element={<LandingContainer signOut={() => signOut()} />}
-            />
-            <Route path="/details" element={<Details />} />
-          </Routes>
-        </React.Fragment>
-      )}
-    </Authenticator>
+    <React.Fragment>
+      <Routes>
+        <Route path="/" element={<LandingContainer signOut={signOut} />} />
+        <Route path="/details" element={<Details />} />
+      </Routes>
+    </React.Fragment>
+  );
+};
+
+function App() {
+  const { authStatus } = useAuthenticator((context) => [context.authStatus]);
+  // Use the value of authStatus to decide which page to render
+  return (
+    <>
+      {authStatus === "configuring" && "Loading..."}
+      {authStatus !== "authenticated" ? <AppAuthenticator /> : <Home />}
+    </>
   );
 }
 
